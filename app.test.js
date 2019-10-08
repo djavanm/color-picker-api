1b1347aa30882508a7d363566348204d6c05a18b
Chris Lane <pankokrumbs@gmail.com>
2019-10-08 16:22:20 -0600

    Add all the GET endpoints and corresponding tests Co-Author: Djavan Munroe <DjavanM24@gmail.com>

--- app.test.js
+++ app.test.js
@@ -39,6 +39,38 @@ describe('Server', () => {
       expect(response.status).toBe(400);
       expect(response.body.error).toEqual("Unable to verify user.");
     });
-  
   });
+
+  describe('GET /projects/:id', () => {
+    it('should return a 200 status code and all the projects associated with the user via user_id', async () => {
+      const user = await database('users').select().first();
+      const response = await request(app).get(`/projects/${user.id}`);
+      const expectedProjects = await database('projects').where('user_id', user.id).select();
+      expect(response.body[0].name).toEqual(expectedProjects[0].name);
+    })
+  })
+
+  describe('GET /palettes/:id', () => {
+    it('should return a 200 status code and all the palettes associated with the project via project_id', async () => {
+      const { id } = await database('projects').first();
+      const response = await request(app).get(`/palettes/${id}`);
+      const expectedPalettes = await database('palettes').where('project_id', id).select();
+      const { name, hex_codes } = response.body[0];
+
+      expect(name).toEqual(expectedPalettes[0].name)
+      expect(hex_codes).toEqual(expectedPalettes[0].hex_codes)
+    })
+  })
+
+  describe('GET /palettes', () => {
+    it('should return a 200 status code and all the palettes ', async () => {
+      const response = await request(app).get('/palettes');
+      const expectedPalettes = await database('palettes').select();
+      const { name, hex_codes } = response.body[0];
+
+      expect(name).toEqual(expectedPalettes[0].name)
+      expect(hex_codes).toEqual(expectedPalettes[0].hex_codes)
+    })
+  })
+
 });
