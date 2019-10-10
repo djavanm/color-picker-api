f946ca92f4fec07e9da3af318136171bb2bf1e08
Djavan Munroe <djavanm24@gmail.com>
2019-10-10 11:48:24 -0600

    Fix port on server.js Co-Authored-By: Chris Lane <PankoKrumbs@gmail.com>

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-app.set('port', 3001);
+app.set('port', process.env.PORT || 3001)
 
 app.listen(app.get('port'), () => {
   console.log(`App is listening on port ${app.get('port')}`)
